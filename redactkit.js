0c71a66bfa669574f5aab70f3601f18c83d2f979
Alex Arena <alex@arena.im>
2016-10-11 20:00:12 -0700

    Initial commit

---
+++ redactkit.js
@@ -0,0 +1,17 @@
+module.exports.redact = function redact(text,ignoreSpaces,start,stop){
+  var output = "";
+  for(var i=0; i<text.length; i++){
+    if(i < start || i> stop){
+      output += text.charAt(i);
+    }
+    else{
+      if((text.charAt(i) != ' '|| ignoreSpaces) && text.charAt(i) != '\n'){
+        output += "\u2588";
+      }
+      else{
+        output += text.charAt(i);
+      }
+    }
+  }
+  return output;
+}
---
+++ sample.js
@@ -0,0 +1,3 @@
+var redactkit = require('./node_modules/redactkit.js');
+var sample = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam\nvarius suscipit commodo. Quisque lobortis libero sed mauris tincidunt\nplacerat semper nec enim. Phasellus congue leo magna,id\neuismod risus malesuada et. Nunc porttitor ac enim non finibus.\nNulla hendrerit magna eu aliquet porttitor.'
+console.log(redactkit.redact(test));
