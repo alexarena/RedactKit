4502874e604dfa3122c30079f5014ea479fc4256
Alex Arena <alex@arena.im>
2016-10-11 20:18:25 -0700

    actually fixed the sample program

--- sample.js
+++ sample.js
@@ -1,3 +1,3 @@
 var redactkit = require('redactkit');
-var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam\nvarius suscipit commodo. $
+var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam\nvarius suscipit commodo. Quisque lobortis libero sed mauris tincidunt\nplacerat semper nec enim. Phasellus congue leo magna,id\neuismod risus malesuada et. Nunc porttitor ac enim non finibus.\nNulla hendrerit magna eu aliquet porttitor.'
 console.log(redactkit.redact(text));
